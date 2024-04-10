0360aac6e8c7d69620eb7c53e5067e301694b079
delaylee <delaylee219@gmail.com>
2024-04-10 15:57:23 +0900

    Init Project

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -14,10 +20,12 @@ public class UserController {
 
     private final Environment env;
     private final Greeting greeting;
+    private final UserService userService;
 
-    public UserController(Environment env, Greeting greeting) {
+    public UserController(Environment env, Greeting greeting, UserService userService) {
         this.env = env;
         this.greeting = greeting;
+        this.userService = userService;
     }
 
     @GetMapping("/health_check")
@@ -29,5 +37,16 @@ public class UserController {
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        UserDto response = userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(response, ResponseUser.class);
+        return new ResponseEntity(responseUser, HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.userservice.service;
+
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.repository.UserEntity;
+import com.example.userservice.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString()); // unique
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_pwd");
+
+        userRepository.save(userEntity);
+
+        return mapper.map(userEntity, UserDto.class);
+    }
+}
